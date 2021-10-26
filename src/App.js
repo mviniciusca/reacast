e1c6d3a8136f6d6918f0aa09c23871fa6db6da47
Marcos Vinícius <mviniciusca@gmail.com>
2021-10-26 01:23:51 -0300

    adicionado axios para consulta com api do aerodatabox

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+
 import { useState } from 'react';
 import './App.css';
 import Footer from './components/Footer/Footer.js';
-//import Localfinder from './components/Localfinder/Localfinder';
 
 function App() {
 
-    //Geolocalização Google Maps
-
-
+    //Geolocalização Google API
     window.onload = function () {
         var startPos;
         var geoSuccess = function (position) {
@@ -17,38 +15,33 @@ function App() {
 
             const geoPositionSetter = latitude + ',' + longitude;
             setGeoPosition(geoPositionSetter);
+
+            getLatitude(latitude);
+            getLongitude(longitude);
+
         };
         navigator.geolocation.getCurrentPosition(geoSuccess);
     };
 
     //recebe dados da api da previsão do tempo
-
     const API_URL = process.env.REACT_APP_API_URL
     const API_KEY = process.env.REACT_APP_API_KEY
     const API_LANG = process.env.REACT_APP_API_LANG
     const API_AQI = process.env.REACT_APP_API_AQI
 
-
-    // recebe os dados da api do aeroporto
-
-    const USER_LAT = "-22.734518";
-    const USER_LONG = "-43.319799";
-    const API_MAX_AIRPORTS = "3";
-    const API_ONLY_FLIGHTS = true;
-    const API_RADIUS = "100";
+    //geolocalization state
+    const [getLat, getLatitude] = useState(null);
+    const [getLon, getLongitude] = useState(null);
 
     // tratamento de erros
-
     const [error400, setError400] = useState("");
     const [error401, setError401] = useState("");
     const [errorUnexpected, setErrorUnxpected] = useState("");
 
-
     //exibe o app
     const [hiddenApp, setHiddenApp] = useState(false);
 
-    // Initial State 
-
+    //initial State 
     const [homepage, setHomepage] = useState(false);
     const handleHomepage = () => {
         setHomepage(true);
@@ -58,10 +51,9 @@ function App() {
     const [buscaCidade, setBuscaCidade] = useState("");
 
     // recebe os dados da api 
-    const [forecast, setForecast] = useState(null)
+    const [forecast, setForecast] = useState(null);
 
-    //
-    // const [aeroporto, setAirport] = useState(null)
+    // const [airport, setAirport] = useState([]);
 
     // feedback positivo
     const [feedbackPositivo, setFeedbackPositivo] = useState(false);
@@ -93,25 +85,19 @@ function App() {
     const [thirdCity, setThirdCity] = useState(true);
 
     const handleFirstTrendCities = () => {
-
         setFirstCity("Rio de Janeiro");
         setBuscaCidade("Rio de Janeiro");
-
-
     }
 
     const handleSecondTrendCities = () => {
         setSecondCity("Paris");
         setBuscaCidade("Paris");
-
     }
 
     const handleThirdTrendCities = () => {
         setThirdCity("Nova York");
         setBuscaCidade("New York");
-
     }
-
 
     //geoposition
     const [geoPosition, setGeoPosition] = useState(null);
@@ -159,31 +145,15 @@ function App() {
 
         });
 
-        // AeroDataBox 
-        fetch(`https://aerodatabox.p.rapidapi.com/airports/search/location/${USER_LAT}/${USER_LONG}/km/${API_RADIUS}/${API_MAX_AIRPORTS}?withFlightInfoOnly=${API_ONLY_FLIGHTS}`, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
-                "x-rapidapi-key": "e4eac7ba6cmsh1ac573be6029344p12126cjsn9e7c3aefe57b"
-            }
-        })
-            .then(response => {
-                return response.json();
-
-            })
-            .catch(err => {
-                console.error(err);
-            });
     }
 
-    // Retorna o App em HTML
+    // app
     return (
         <>
 
-            <div className=
-                {error400 || error401 || errorUnexpected ? "animate__animated animate__wobble  app-bloco class-error" : "app-bloco"}
-            >
-
+            <div className={
+                error400 || error401 || errorUnexpected ? "animate__animated animate__wobble  app-bloco class-error" : "app-bloco"
+            }>
                 <div className="hiddenApp">
                     <div className="app-logo">
                         <div className="icon-logo">
@@ -205,7 +175,7 @@ function App() {
                     <div className="app-finder-wrapper">
                         <div className="app-finder-title">Ou</div>
                         <div className="app-finder-map">
-                            <div className="animate__animated animate__shakeX animate__infinite	infinite">
+                            <div className="animate__animated animate__shakeX animate__repeat-3">
                                 <ion-icon id="map-search-icon" onClick={handleGeoSearch} value={geoPosition} name="locate-outline"></ion-icon>
                             </div>
                         </div>
@@ -224,12 +194,9 @@ function App() {
                         </span>
                     </div>
                     <div className="app-error-handler">
-
                         {error400 ? 'Busque por uma localização válida. Erro 400' : null}
                         {error401 ? 'Ocorreu um erro com a API. Erro 401' : null}
                         {errorUnexpected ? 'Erro Inesperado!' : null}
-
-
                     </div>
                 </div>
 
@@ -244,7 +211,7 @@ function App() {
                             <div className="app-return-text">{forecast.current.condition.text}</div>
                             <div className="app-return-city">{forecast.location.name}, {forecast.location.country}</div>
                             <div className="app-feedback">localização correta?</div>
-                            <div>  </div>
+                            <div className="app-airport-wrapper"></div>
                             <div className="app-return-thumbs">
                                 {feedbackNegativo ? '' :
                                     <ion-icon id="thumbs-up" value={feedbackPositivo} onClick={handleFeedback} name="thumbs-up-outline"></ion-icon>
@@ -265,7 +232,6 @@ function App() {
 
             </div>
             <Footer />
-
         </>
     );
 }
